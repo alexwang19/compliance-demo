ff6aaadc246e47f7b2866111d8ba8645b4fe8a55
Alex Wang <alex.wang@sysdig.com>
2023-09-04 10:52:54 -0500

    add test file

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "sko_bucket" {
+  bucket = "sysdig-sko-bucket-1234"
+}
+
+resource "aws_s3_bucket_public_access_block" "sko_bucket" {
+  bucket = aws_s3_bucket.sko_bucket.id
+  block_public_acls = true
+  block_public_policy = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
